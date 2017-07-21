facb861b55911846124b9e48ec283f73a56ec3d4
Rex Roof <rexroof@gmail.com>
2017-07-20 22:20:05 -0400

    script for demonstrating snippets in shell

---
+++ jenkins-scripts/snippet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILE=${1:-/etc/passwd}
+FILENAME=$(basename $FILE)
+
+JSON=$(jq -Rs "{ filename: \"$FILENAME\", content: . }" $FILE)
+
+curl -XPOST -H"Content-Type: application/json" \
+  -w "\n" \
+  -d "${JSON}" \
+  https://hambot.rexroof.com/snippet/button
